e33c5ac1a36f7f392e220255b4c570a184b92515
Cecile Varin <varincecile@hotmail.com>
2017-11-23 15:51:33 +0100

    db seeded with JSON random ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+list_serialized = open(url).read
+ingredient = JSON.parse(list_serialized)
+ingredient["drinks"].sample(3).each do |ingredient|
+  Ingredient.create(name: "#{ingredient["strIngredient1"]}")
+end
+
+
+# puts "#{ingredient["strIngredient1"]}"
+# puts "#{"drinks"[1]["strIngredient1"]}"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
