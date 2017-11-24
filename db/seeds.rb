d85b2153fe2474b985d445b14ff2b06def459b42
Cecile Varin <varincecile@hotmail.com>
2017-11-24 14:24:30 +0100

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,108 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'json'
-require 'open-uri'
+# My first seed below used an API, then I used The Wagon seeding file
+# Don't forget to add the destroy_all before seeding
+# require 'json'
+# require 'open-uri'
 
 
-url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-list_serialized = open(url).read
-ingredient = JSON.parse(list_serialized)
-ingredient["drinks"].sample(3).each do |ingredient|
-  Ingredient.create(name: "#{ingredient["strIngredient1"]}")
-end
+# url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# list_serialized = open(url).read
+# ingredient = JSON.parse(list_serialized)
+# ingredient["drinks"].sample(3).each do |ingredient|
+# Ingredient.create(name: "#{ingredient["strIngredient1"]}")
+# end
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+cocktails = [
+  {
+    name: "Old Fashioned",
+    # picture: "http://www.seriouseats.com/images/2014/11/20141104-cocktail-party-old-fashioneds-holiday-vicky-wasik-3.jpg"
+    },
+  {
+    name: "Daiquiri",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-daiquiri.jpg"
+    },
+  {
+    name: "Margarita",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-margarita.jpg"
+    },
+  {
+    name: "Sidecar",
+    # picture: "http://www.seriouseats.com/images/2014/11/20141101-cognac-sidecar-carey-jones.jpg"
+    },
+  {
+    name: "French 75",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-french75.jpg"
+    },
+  {
+    name: "Bloody Mary",
+    # picture: "http://www.seriouseats.com/images/2015/03/twase-20150320-21.jpg"
+    },
+  {
+    name: "Irish Coffee",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-irish-coffee.jpg"
+    },
+  {
+    name: "Jack Rose",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-jack-rose.jpg"
+    },
+  {
+    name: "Negroni",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-negroni.jpg"
+    },
+  {
+    name: "Boulevardier",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-boulevardier.jpg"
+    },
+  {
+    name: "Sazerac",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-sazerac.jpg"
+    },
+  {
+    name: "Vieux Carré",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-vieux-carre.jpg"
+    },
+  {
+    name: "Ramos Gin Fizz",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-robyn-lee-ramos-gin-fizz.jpg"
+    },
+  {
+    name: "Mint Julep",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-mint-julep.jpg"
+    },
+  {
+    name: "Whiskey Sour",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-whiskey-sour.jpg"
+    },
+  {
+    name: "Mai Tai",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-mai-tai.jpg"
+    },
+  {
+    name: "Planter's Punch",
+    # picture: "http://www.seriouseats.com/images/2015/04/20150406-cocktails-planters-punch-robyn-lee-1.jpg"
+    },
+  {
+    name: "Pisco Sour",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-pisco-sour.jpg"
+    },
+  {
+    name: "Cosmopolitan",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-cosmopolitan.jpg"
+    },
+  {
+    name: "Tom Collins",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-tom-collins.jpg"
+    },
+  {
+    name: "Last Word",
+    # picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-last-word.jpg"
+  }
+]
+
+ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
+ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
 
 
-# puts "#{ingredient["strIngredient1"]}"
-# puts "#{"drinks"[1]["strIngredient1"]}"
-
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
+cocktails.each { |cocktail| Cocktail.create(cocktail) }
